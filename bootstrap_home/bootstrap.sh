10638c63999df334683dd3279cfd2455c250f4ee
Ziyue Li <liziyue241@gmail.com>
2020-05-21 16:56:52 -0400

    first success test of script on new mac

--- bootstrap_home/bootstrap.sh
+++ bootstrap_home/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 export CODE_HOME="$HOME/Code"
 export BOOTSTRAP_HOME="$CODE_HOME/bootstrap"
 export DOTFILES_HOME="$CODE_HOME/dotfiles"
@@ -8,16 +8,18 @@ export DOTFILES_PRIVATE_HOME="$DOTFILES_HOME/dotfiles_private"
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # brew install the rest of the packages
-cd $BOOTSTRAP_HOME
+cd $BOOTSTRAP_HOME/bootstrap_home
 brew bundle
-source ~/.bash_profile
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # link all dotfiles
 if [[ ! -d $DOTFILES_HOME/dotfiles_home ]]; then
 	git clone git@github.com:feynlee/dotfiles.git ~/Code/dotfiles/dotfiles_home
 fi
 
-if [[ ! -d $DOTFILES_HOME/dotfiles_private ]]; then
+if [[ ! -d $DOTFILES_HOME/dotfiles_tools_li ]]; then
 	git clone git@github.com:feynlee/dotfiles_tools_li.git ~/Code/dotfiles/dotfiles_tools_li
 fi
 
@@ -27,12 +29,12 @@ if [[ ! -d $DOTFILES_PRIVATE_HOME ]]; then
 fi
 
 rm -rf ~/.ssh
-cd $DOTFILES_HOME/dotfiles_private && stow -R -v -t . && git pull
-
-cd $DOTFILES_HOME/dotfiles_home && stow -R -v -t . && git pull
-cd $DOTFILES_HOME/dotfiles_tools_li && stow -R -v -t . && git pull
+rm -rf ~/.zshrc
+cd $DOTFILES_HOME/dotfiles_private && stow -R -v -t ~ .
+cd $DOTFILES_HOME/dotfiles_tools_li && git pull && stow -R -v -t ~ .
+cd $DOTFILES_HOME/dotfiles_home && git pull && stow -R -v -t ~ .
 . ~/.bash_profile
-
+. ~/.zprofile
 
 # setup Vundle for vim
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -42,6 +44,7 @@ cd $DOTFILES_HOME/dotfiles_tools_li && stow -R -v -t . && git pull
 # so that it's using the correctly linked system python)
 # cd ~/.vim/bundle/YouCompleteMe
 # ./install.py
+
 # install airline fonts
 cd ~
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -53,17 +56,19 @@ rm -rf fonts
 
 
 # install anaconda
-if [[ ! -f ~/anaconda3.sh ]]; then
-	cd ~
-	wget https://repo.continuum.io/archive/Anaconda3-5.1.0-MacOSX-x86_64.sh -O ~/anaconda3.sh
-	bash ~/anaconda3.sh -b -p /Applications/Anaconda3
-	rm ~/anaconda3.sh
+if [[ ! -f ~/Downloads/anaconda3.sh ]]; then
+	wget https://repo.anaconda.com/archive/Anaconda3-2020.02-MacOSX-x86_64.sh -O ~/Downloads/anaconda3.sh
 fi
-# export PATH="Applications/anaconda3/bin:$PATH" >> ~/.bashrc
+/bin/bash ~/Downloads/anaconda3.sh -b -p -u /Applications/Anaconda3
+rm ~/Downloads/anaconda3.sh
+source /Applications/Anaconda3/bin/activate
+conda init
+conda config --set auto_activate_base True
+
 # use conda to install all python packages
 conda install -y nb_conda_kernels
-conda env create -f $DOTFILES_HOME/py37.yml
-conda install pyspark
+conda env create -y -f py37.yml
+conda install -y pyspark
 # install notebook extensions
 pip install --upgrade pip
 pip install jupyter_contrib_nbextensions
@@ -71,7 +76,5 @@ jupyter contrib nbextension install --user
 
 # install spark
 # wget -O
-
-
 
 echo "==> All done!"
